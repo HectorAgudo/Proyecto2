a2e75e789a97c43ceb95ff520c4fd2ca63fbb2b1
Hector <hector.agudo@gmail.com>
2022-04-30 16:39:33 +0200

    medio funciona la base de datos

--- app/src/main/java/com/example/dydproyect/entidades/Personaje.java
+++ app/src/main/java/com/example/dydproyect/entidades/Personaje.java
@@ -3,7 +3,7 @@ package com.example.dydproyect.entidades;
 public class Personaje {
 
     private String nombre;
-    private int exp;
+    private String exp;
     private int nivel;
     private String clase;
     private String raza;
@@ -13,7 +13,126 @@ public class Personaje {
     private int inteligencia;
     private int sabiduria;
     private int carisma;
+    private String alineamiento;
 
 
+    public String getAlineamiento() {
+        return alineamiento;
+    }
 
+    public void setAlineamiento(String alineamiento) {
+        this.alineamiento = alineamiento;
+    }
+
+    public Personaje(){
+
+    }
+
+    public Personaje(String nombre, String exp, int nivel, String clase, String raza, int fuerza, int destreza, int constitucion, int inteligencia, int sabiduria, int carisma, String alineamiento) {
+        this.nombre = nombre;
+        this.exp = exp;
+        this.nivel = nivel;
+        this.clase = clase;
+        this.raza = raza;
+        this.fuerza = fuerza;
+        this.destreza = destreza;
+        this.constitucion = constitucion;
+        this.inteligencia = inteligencia;
+        this.sabiduria = sabiduria;
+        this.carisma = carisma;
+        this.alineamiento = alineamiento;
+    }
+
+    public Personaje(String nombre, String exp) {
+        this.nombre = nombre;
+        this.exp = exp;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setExp(String exp) {
+        this.exp = exp;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public void setConstitucion(int constitucion) {
+        this.constitucion = constitucion;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public void setSabiduria(int sabiduria) {
+        this.sabiduria = sabiduria;
+    }
+
+    public void setCarisma(int carisma) {
+        this.carisma = carisma;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getExp() {
+        return exp;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public int getConstitucion() {
+        return constitucion;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public int getSabiduria() {
+        return sabiduria;
+    }
+
+    public int getCarisma() {
+        return carisma;
+    }
 }
--- app/src/main/java/com/example/dydproyect/vistas/jugador/JugadorNombre.java
+++ app/src/main/java/com/example/dydproyect/vistas/jugador/JugadorNombre.java
@@ -6,22 +6,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dydproyect.R;
+import com.example.dydproyect.entidades.Personaje;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JugadorNombre extends AppCompatActivity {
 
     Button btnActualizar, btnVida, btnAtributos, btnHabilidades, btnSalvaciones;
     EditText editNombre, editPuntosExp;
-
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nombre);
+
+
 
         editNombre = findViewById(R.id.editTextNombrePj);
         editPuntosExp = findViewById(R.id.editTextPExp);
@@ -31,6 +42,11 @@ public class JugadorNombre extends AppCompatActivity {
         btnAtributos = findViewById(R.id.buttonAtribnom);
         btnHabilidades = findViewById(R.id.buttonHabiNom);
         btnSalvaciones = findViewById(R.id.buttonSalvaNom);
+        btnActualizar = findViewById(R.id.buttonActuNombre);
+
+        inicializarFirebase();
+        //deCero();
+        listarPj();
 
         btnVida.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +72,49 @@ public class JugadorNombre extends AppCompatActivity {
                 irSalvaciones();
             }
         });
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Personaje personaje = new Personaje(editNombre.getText().toString(),editPuntosExp.getText().toString());
+                databaseReference.child("Personaje").setValue(personaje);
+            }
+        });
+
+
+
+    }
+    private void deCero(){
+        Personaje personaje = new Personaje("Introducir nombre pj","0");
+        databaseReference.child("Personaje").setValue(personaje);
+
+    }
+
+
+    private void listarPj() {
+        databaseReference.child("Personaje").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String nombre = snapshot.child("nombre").getValue().toString();
+                    editNombre.setText(nombre);
+                    String exp = snapshot.child("exp").getValue().toString();
+                    editPuntosExp.setText(exp);
+
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 
     private void irAtributos(){
